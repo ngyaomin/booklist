2caa135adab7fae917dd1fd7277ad7f94258989b
Yao Min <ngyaominsg@gmail.com>
2018-05-12 19:46:52 +0800

    Add book to list es6

--- appes6.js
+++ appes6.js
@@ -7,19 +7,30 @@ class Book {
 }
 
 class UI {
-  addBookToList() {
+  addBookToList(book) {
+    const list = document.getElementById('book-list');
+    // create tr element
+    const row = document.createElement('tr');
+    // insert cols
+    row.innerHTML = `
+      <td>${book.title}</td>
+      <td>${book.author}</td>
+      <td>${book.isbn}</td>
+      <td><a href="#" class="delete">X</a></td>
+    `;
+
+    list.appendChild(row);
+  }
+
+  showAlert(message, className) {
 
   }
 
-  showAlert() {
-
-  }
-
-  deleteBook() {
+  deleteBook(target) {
 
   }
 
   clearFields() {
-    
+
   }
 }
