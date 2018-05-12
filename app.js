338ef5af907aa20de45caf75f207dbb42eae349f
Yao Min <ngyaominsg@gmail.com>
2018-05-12 18:25:27 +0800

    More debugging

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ UI.prototype.showAlert = function(message, className) {
   div.appendChild(document.createTextNode(message));
 
   // get parent
-  const container = document.querySelector('container');
+  const container = document.querySelector('.container');
   const form = document.querySelector('#book-form');
   // insert alert
   container.insertBefore(div, form);
